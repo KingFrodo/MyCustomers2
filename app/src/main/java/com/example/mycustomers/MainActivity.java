b99d9e665fae2b7e8fac3ca377495bcb8bb8e3a6
KingFrodo <foro.lukas@liwest.at>
2019-02-28 08:21:21 +0100

    fifth commit

--- app/src/main/java/com/example/mycustomers/MainActivity.java
+++ app/src/main/java/com/example/mycustomers/MainActivity.java
@@ -72,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private InputStream getInputStreamForAsset(String filename){
+        //testen
         AssetManager assets = getAssets();
 
         try {
